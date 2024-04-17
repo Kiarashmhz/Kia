44ec7a97d055c6ec659e3af4e4921c90b14b398b
Kia <kiarashhosseinzadehhhh@gmail.com>
2024-04-17 12:01:30 +0330

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<form action="#" method="post">
+    <input type="number" name="num">
+    <button type="submit">save</button>
+</form>
+</html>
+<?php
+ $a=(int)$_POST['num'];
+ $b=1;
+for ($c=$a;$c>=1;$c--){
+    $b=$b*$c;
+}
+echo "Factorial of $a is $b";
+?>
+
